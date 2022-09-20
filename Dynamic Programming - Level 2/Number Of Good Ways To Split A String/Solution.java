7b9f6fdf1f9dc4db76f5d24d11abaacbf0ba5e16
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2022-09-20 17:23:54 +0530

    Update Solution.java

--- Dynamic Programming - Level 2/Number Of Good Ways To Split A String/Solution.java
+++ Dynamic Programming - Level 2/Number Of Good Ways To Split A String/Solution.java
@@ -1,1 +1,39 @@
+class Solution {
+    public int numSplits(String s) {
+       int n= s.length();
+       int[] pre = new int [n];
+       int[] suf = new int [n];
+       int[] fre = new int [26];
+       int count =0;
+       for(int i=0; i < n; i++){
+           int ch = s.charAt(i) - 'a';
+           fre[ch]++;
+           if(fre[ch] == 1){
+               count++;
+           }
+           pre[i] = count;
+       }
 
+       fre = new int [26];
+       count = 0;
+
+       for(int i=n-1; i >= 0; i--){
+           int ch = s.charAt(i) - 'a';
+           fre[ch]++;
+           if(fre[ch] == 1){
+               count++;
+           }
+           suf[i] = count;
+       }
+
+        int splits = 0;
+
+       for(int i=0; i < n-1; i++){
+           if(pre[i] == suf[i+1]){
+               splits++;
+           }
+       }
+       
+       return splits;
+    }
+}
